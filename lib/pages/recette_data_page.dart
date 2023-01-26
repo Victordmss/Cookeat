8a45f70e824f90442a2f16ebd13c5494d73ef6fd
Victor <victor.demessance@gmail.com>
2023-01-26 08:47:42 +0100

    Ajout de la possibilité de créer une recette, avec possibilité de déposer une image et des données dynamiques sur sa base de donnée personnelle.
    A faire :
    - Visualisation unique de ses recettes
    - Affichage des détails d'une recette

---
+++ lib/pages/recette_data_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecetteDataPage extends StatefulWidget {
+  const RecetteDataPage({Key? key}) : super(key: key);
+
+  @override
+  State<RecetteDataPage> createState() => _RecetteDataPageState();
+}
+
+class _RecetteDataPageState extends State<RecetteDataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
